b254afb38c309fa5862c1855c0f7254e60c7ba68
Remi Cartier <rcartier@provenancetech.io>
2021-03-19 11:56:13 -0400

    Working example

--- examples/PythonBackendApp/client_backend.py
+++ examples/PythonBackendApp/client_backend.py
@@ -1,31 +1,52 @@
 import argparse
 import logging
+import json
 
 from flask import Flask, request
+from flask_socketio import SocketIO, emit
 
-from pti_tools import make_signed_request, environ_or_default, environ_or_required, set_logging_level, JWKType
+from pti_tools import make_signed_request, environ_or_default, environ_or_required, set_logging_level, JWKType, decrypt_verify_and_get_payload
 
 p = argparse.ArgumentParser()
-p.add_argument('-l', '--log-level', help='Output debug information to stderr', **environ_or_default('LOG_LEVEL', 'DEBUG'))
-p.add_argument('-sk', '--private-key', help='Path to the private/secret key', type=JWKType(), **environ_or_required('PRIVATE_KEY_PATH'))
+p.add_argument('-l', '--log-level', help='Output debug information to stderr',
+               **environ_or_default('LOG_LEVEL', 'DEBUG'))
+p.add_argument('-pk', '--public-key', help='Path to PTI\'s public key', type=JWKType(), **environ_or_required('PUBLIC_KEY_PATH'))
+p.add_argument('-sk', '--private-key', help='Path to the private/secret key', type=JWKType(),
+               **environ_or_required('PRIVATE_KEY_PATH'))
 p.add_argument('-c', '--client-id', help='PTI Client ID', **environ_or_required('CLIENT_ID'))
-p.add_argument('-u', '--pti-api-base-url', help='PTI API base URL', **environ_or_default('PTI_API_BASE_URL', 'https://pti.apidev.pticlient.com/v0'))
+p.add_argument('-u', '--pti-api-base-url', help='PTI API base URL',
+               **environ_or_default('PTI_API_BASE_URL', 'https://pti.apidev.pticlient.com/v0'))
 args = p.parse_args()
 set_logging_level(args.log_level)
 
 app = Flask('example_passthrough', static_url_path='')
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/generateToken', methods=['POST'])
 def generate_token():
     payload = request.get_json()
-    logging.info(payload)
-    response = make_signed_request(args.client_id, args.private_key, f'{args.pti_api_base_url}/auth/userToken', method="POST", data=str(payload['x-pti-token-payload']))
-    logging.info(response)
-    return response.json()['accessToken']
+    json_data = json.dumps(payload['x-pti-token-payload'])
+    response = make_signed_request(args.client_id, args.private_key, f'{args.pti_api_base_url}/auth/userToken',
+                                   method="POST", data=json_data)
+    return response.json()
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    #this would typically be running elsewhere
+    logging.warning('Got webhook POST')
+    encrypted_and_signed = request.get_json()
+
+    payload = decrypt_verify_and_get_payload(args.private_key, args.public_key, json.dumps(encrypted_and_signed))
+    json_payload = json.loads(payload)
+    if type(json_payload) is str:
+        json_payload = json.loads(json_payload)
+    logging.warning(f'Payload:\n{json_payload})
+    logging.warning('Successfully decrypted and verified payload')
+    socketio.emit(json_payload['requestId'], {'data': 'OK'})
+    return '', 204
 
 def run_server():
-    app.run(port=8000, threaded=True, host='0.0.0.0')
+    socketio.run(app)
 
 if __name__ == "__main__":
     run_server()
-
--- examples/ReactJsSdkApp/src/App.js
+++ examples/ReactJsSdkApp/src/App.js
@@ -8,109 +8,118 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import TextField from '@material-ui/core/TextField';
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
+import io from "socket.io-client";
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
 function SimpleDialog(props) {
-  const { open, callback, type, userId, requestId } = props;
-  const [ sdkInit, setSdkInit ] = React.useState(false);
+    const {open, callback, type, userId, requestId, amount} = props;
+    const [sdkInit, setSdkInit] = React.useState(false);
 
-  const setRef = (e) => {
-    if (e != null && !sdkInit) {
-        setSdkInit(true);
-        switch (type) {
-            case 'FIAT_FUNDING':
-                 PTI.form({
-                     type: "FIAT_FUNDING",
-                     requestId: requestId,
-                     userId: userId,
-                     amount: "34.00",
-                     parentElement: document.getElementById(e.id),
-                     callback: callback
-                 })
-                break;
-            case 'KYC':
-                PTI.form({
-                    type: "KYC",
-                    requestId: requestId,
-                    userId: userId,
-                    parentElement: document.getElementById(e.id),
-                    callback: callback
+    const setRef = (e) => {
+        if (e != null && !sdkInit) {
+            setSdkInit(true);
+            switch (type) {
+                case 'FIAT_FUNDING':
+                    PTI.form({
+                        type: "FIAT_FUNDING",
+                        requestId: requestId,
+                        userId: userId,
+                        amount: amount,
+                        parentElement: document.getElementById(e.id),
+                        callback: callback
+                    })
+                    break;
+                case 'KYC':
+                    PTI.form({
+                        type: "KYC",
+                        requestId: requestId,
+                        userId: userId,
+                        parentElement: document.getElementById(e.id),
+                        callback: callback
                     });
-                break;
+                    break;
+            }
         }
     }
-  }
 
-  return (
-    <Dialog open={open} fullScreen={true}>
-        <DialogTitle>{type}</DialogTitle>
-        <DialogContent>
-            <div id={ type + "PlaceHolder" } ref={setRef} style={{width:'550px',height:'1200px'}}>
-            </div>
-        </DialogContent>
-    </Dialog>
-  );
+    return (
+        <Dialog open={open} fullScreen={true}>
+            <DialogTitle>{type} - {amount} - {requestId}</DialogTitle>
+            <DialogContent>
+                <div id={type + "PlaceHolder"} ref={setRef} style={{width: '550px', height: '1200px'}}>
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
 }
 
 export default function App() {
-  const [paymentOpen, setPaymentOpen] = React.useState(false);
-  const [kycOpen, setKycOpen] = React.useState(false);
-  const [userId, setUserId] = React.useState("9b83516d-1810-4e77-86fc-a4cad0a4bf94");
-  const [requestId, setRequestId] = React.useState(uuidv4());
+    const [paymentOpen, setPaymentOpen] = React.useState(false);
+    const [kycOpen, setKycOpen] = React.useState(false);
+    const [userId, setUserId] = React.useState("9db9738d-1b41-4fd8-8536-319be308d9f2");
+    const [requestId, setRequestId] = React.useState(uuidv4());
+    const [amount, setAmount] = React.useState('' + Math.round(Math.random() * 100) + '.' + Math.round(Math.random() * 100));
 
-  const handlePayment = () => {
-    setPaymentOpen(true);
-  };
+    let socket = io.connect("http://localhost:5000");
 
-  const handleKyc = () => {
-    setKycOpen(true);
-  };
+    React.useEffect(() => {
+        socket.on(requestId, (msg) => {
+            console.log(msg);
+            setKycOpen(false);
+            setPaymentOpen(false);
+        });
+    }, [requestId])
 
-  const handlePaymentCallback = () => {
-    setPaymentOpen(false);
-  };
+    const handlePayment = () => {
+        setPaymentOpen(true);
+    };
 
-  const handleKycCallback = () => {
-    setKycOpen(false);
-  };
+    const handleKyc = () => {
+        setKycOpen(true);
+    };
 
-  const handleUserIdChange = (e) => {
-    setUserId(e.target.value);
-  }
+    const handlePaymentCallback = () => {
+        setPaymentOpen(false);
+    };
 
-  const handleRequestIdChange = (e) => {
-    setRequestId(e.target.value);
-  }
+    const handleKycCallback = () => {
+        setKycOpen(false);
+    };
 
-  return (
-    <Container>
-      <Box my={4}>
-        {/*<Typography align="center" gutterBottom={true} >*/}
-            <TextField id={"userId"} value={userId} onChange={handleUserIdChange} label={'UserId'} fullWidth={true}/>
-            <br/><br/>
-            <TextField id={"requestId"} value={requestId} onChange={handleRequestIdChange} label={'RequestId'} fullWidth={true}/>
-            <br/><br/>
-            <Button variant="contained" onClick={handlePayment} fullWidth={true}>Open Payment Form</Button><br/><br/>
-            <Button variant="contained" onClick={handleKyc} fullWidth={true}>Open KYC Form</Button>
-        {/*</Typography>*/}
-        <br/>
-        <Copyright />
-      </Box>
-    <SimpleDialog open={paymentOpen} callback={handlePaymentCallback} type="FIAT_FUNDING" userId={userId}/>
-    <SimpleDialog open={kycOpen} callback={handleKycCallback} type="KYC" userId={userId}/>
-    </Container>
-  );
+    return (
+        <Container>
+            <Box my={4}>
+                <TextField id={"userId"} value={userId} onChange={(e) => setUserId(e.target.value)} label={'UserId'}
+                           fullWidth={true}/>
+                <br/><br/>
+                <TextField id={"requestId"} value={requestId} onChange={(e) => setRequestId(e.target.value)}
+                           label={'RequestId'}
+                           fullWidth={true}/>
+                <br/><br/>
+                <TextField id={"amount"} value={amount} onChange={(e) => setAmount(e.target.value)} label={'Amount'}
+                           fullWidth={true}/>
+                <br/><br/>
+                <Button variant="contained" onClick={handlePayment} fullWidth={true}>Open Payment
+                    Form</Button><br/><br/>
+                <Button variant="contained" onClick={handleKyc} fullWidth={true}>Open KYC Form</Button>
+                <br/>
+                <Copyright/>
+            </Box>
+            <SimpleDialog open={paymentOpen} callback={handlePaymentCallback} type="FIAT_FUNDING" userId={userId} amount={amount} requestId={requestId} />
+            <SimpleDialog open={kycOpen} callback={handleKycCallback} type="KYC" userId={userId} requestId={requestId} />
+        </Container>
+    );
 }
