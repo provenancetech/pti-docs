1f4482caaa0c66dc06cdb0a676cd1650189e365c
Remi Cartier <rcartier@provenancetech.io>
2021-06-08 12:38:56 -0400

    Using tabs for transaction monitoring section (transfer/withdrawal/funding)

--- examples/admin-appsync/src/UserTransactionMonitoringTable.js
+++ examples/admin-appsync/src/UserTransactionMonitoringTable.js
@@ -1,10 +1,20 @@
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import {
+    AppBar, Box,
+    Paper,
+    Tab,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow, Tabs, Typography,
+} from "@material-ui/core";
 import {useEffect, useState} from "react";
 import {API, graphqlOperation} from "aws-amplify";
 
 const getMonitorsQuery = `
-query ($user_id: ID!) {
-  getPagedTransactionMonitoringByUser(user_id: $user_id, limit: 500, offset: 0) {
+query ($user_id: ID!, $transactionType: String!) {
+  getPagedTransactionMonitoringByUser(user_id: $user_id, limit: 500, offset: 0, transactionType: $transactionType) {
     request_id
     client_id
     user_id
@@ -42,69 +52,223 @@ query ($user_id: ID!) {
 
 
 const UserTransactionMonitoringTable = (props) => {
-    const [monitors, setMonitors] = useState([]);
+    const [monitorsTransfer, setMonitorsTransfer] = useState([]);
+    const [monitorsWithdrawal, setMonitorsWithdrawal] = useState([]);
+    const [monitorsFunding, setMonitorsFunding] = useState([]);
+    const [tab, setTab] = useState(0);
 
     useEffect(() => {
         if (props.userId == null) {
             return;
         }
         API.graphql(graphqlOperation(getMonitorsQuery, {
-            user_id: props.userId
+            user_id: props.userId,
+            transactionType: 'TRANSFER'
         })).then((obj) => {
-            setMonitors(obj.data.getPagedTransactionMonitoringByUser);
+            setMonitorsTransfer(obj.data.getPagedTransactionMonitoringByUser);
+        }).catch(e => {
+            console.log(e);
+        });
+        API.graphql(graphqlOperation(getMonitorsQuery, {
+            user_id: props.userId,
+            transactionType: 'WITHDRAWAL'
+        })).then((obj) => {
+            setMonitorsWithdrawal(obj.data.getPagedTransactionMonitoringByUser);
+        }).catch(e => {
+            console.log(e);
+        });
+        API.graphql(graphqlOperation(getMonitorsQuery, {
+            user_id: props.userId,
+            transactionType: 'FUNDING'
+        })).then((obj) => {
+            setMonitorsFunding(obj.data.getPagedTransactionMonitoringByUser);
         }).catch(e => {
             console.log(e);
         });
     }, [props.userId])
 
+    const TabPanel = (props) => {
+        const { children, value, index, ...other } = props;
+
+        return (
+            <div
+                role="tabpanel"
+                hidden={value !== index}
+                id={`simple-tabpanel-${index}`}
+                aria-labelledby={`simple-tab-${index}`}
+                {...other}
+            >
+                {value === index && (
+                    <Box p={3}>
+                        <Typography>{children}</Typography>
+                    </Box>
+                )}
+            </div>
+        );
+    }
+
+    const a11yProps = (index) => {
+        return {
+            id: `simple-tab-${index}`,
+            'aria-controls': `simple-tabpanel-${index}`,
+        };
+    }
+
     return (
-        <TableContainer component={Paper}>
-            <Table size="small">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>TransactionType</TableCell>
-                        <TableCell>Date</TableCell>
-                        <TableCell>Amount</TableCell>
-                        <TableCell>UsdValue</TableCell>
-                        <TableCell>Status</TableCell>
-                        <TableCell>Kyc Status</TableCell>
-                        <TableCell>SourceMethod</TableCell>
-                        <TableCell>DestinationMethod</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        monitors.map(monitor => {
-                                const getFormattedMethod = (method) => {
-                                    let result = '-';
-                                    if (method != null) {
-                                        if (method.creditCard != null) {
-                                            result = method.creditCard.first_6 + '...' + method.creditCard.last_4;
-                                        } else if (method.token != null) {
-                                            result = method.token.address + ' (' + method.token.type + ')';
+        <div style={{flexGrow:1}}>
+            <AppBar position={'static'}>
+                <Tabs value={tab} onChange={(e, t) => setTab(t)}>
+                    <Tab label={'TRANSFER'} {...a11yProps(0)}/>
+                    <Tab label={'WITHDRAWAL'} {...a11yProps(1)}/>
+                    <Tab label={'FUNDING'} {...a11yProps(2)}/>
+                </Tabs>
+            </AppBar>
+            <TabPanel value={tab} index={0}>
+                <TableContainer component={Paper}>
+                    <Table size="small">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>TransactionType</TableCell>
+                                <TableCell>Date</TableCell>
+                                <TableCell>Amount</TableCell>
+                                <TableCell>UsdValue</TableCell>
+                                <TableCell>Status</TableCell>
+                                <TableCell>Kyc Status</TableCell>
+                                <TableCell>SourceMethod</TableCell>
+                                <TableCell>DestinationMethod</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                monitorsTransfer.map(monitor => {
+                                        const getFormattedMethod = (method) => {
+                                            let result = '-';
+                                            if (method != null) {
+                                                if (method.creditCard != null) {
+                                                    result = method.creditCard.first_6 + '...' + method.creditCard.last_4;
+                                                } else if (method.token != null) {
+                                                    result = method.token.address + ' (' + method.token.type + ')';
+                                                }
+                                            }
+                                            const shorten = result.substring(0, 20);
+                                            return shorten !== result ? shorten + "..." : shorten;
                                         }
+                                        return (
+                                            <TableRow key={monitor.request_id}>
+                                                <TableCell>{monitor.transactionType}</TableCell>
+                                                <TableCell>{monitor.date.substring(0, 19)}</TableCell>
+                                                <TableCell>{monitor.amount}</TableCell>
+                                                <TableCell>{monitor.usdValue}</TableCell>
+                                                <TableCell>{monitor.status}</TableCell>
+                                                <TableCell>{monitor.kyc_status}</TableCell>
+                                                <TableCell>{getFormattedMethod(monitor.sourceMethod)}</TableCell>
+                                                <TableCell>{getFormattedMethod(monitor.destinationMethod)}</TableCell>
+                                            </TableRow>
+                                        )
                                     }
-                                    const shorten = result.substring(0, 20);
-                                    return shorten !== result ? shorten + "..." : shorten;
-                                }
-                                return (
-                                    <TableRow key={monitor.request_id}>
-                                        <TableCell>{monitor.transactionType}</TableCell>
-                                        <TableCell>{monitor.date.substring(0, 19)}</TableCell>
-                                        <TableCell>{monitor.amount}</TableCell>
-                                        <TableCell>{monitor.usdValue}</TableCell>
-                                        <TableCell>{monitor.status}</TableCell>
-                                        <TableCell>{monitor.kyc_status}</TableCell>
-                                        <TableCell>{getFormattedMethod(monitor.sourceMethod)}</TableCell>
-                                        <TableCell>{getFormattedMethod(monitor.destinationMethod)}</TableCell>
-                                    </TableRow>
                                 )
                             }
-                        )
-                    }
-                </TableBody>
-            </Table>
-        </TableContainer>
+                        </TableBody>
+                    </Table>
+                </TableContainer> </TabPanel>
+            <TabPanel value={tab} index={1}>
+                <TableContainer component={Paper}>
+                    <Table size="small">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>TransactionType</TableCell>
+                                <TableCell>Date</TableCell>
+                                <TableCell>Amount</TableCell>
+                                <TableCell>UsdValue</TableCell>
+                                <TableCell>Status</TableCell>
+                                <TableCell>Kyc Status</TableCell>
+                                <TableCell>SourceMethod</TableCell>
+                                <TableCell>DestinationMethod</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                monitorsWithdrawal.map(monitor => {
+                                        const getFormattedMethod = (method) => {
+                                            let result = '-';
+                                            if (method != null) {
+                                                if (method.creditCard != null) {
+                                                    result = method.creditCard.first_6 + '...' + method.creditCard.last_4;
+                                                } else if (method.token != null) {
+                                                    result = method.token.address + ' (' + method.token.type + ')';
+                                                }
+                                            }
+                                            const shorten = result.substring(0, 20);
+                                            return shorten !== result ? shorten + "..." : shorten;
+                                        }
+                                        return (
+                                            <TableRow key={monitor.request_id}>
+                                                <TableCell>{monitor.transactionType}</TableCell>
+                                                <TableCell>{monitor.date.substring(0, 19)}</TableCell>
+                                                <TableCell>{monitor.amount}</TableCell>
+                                                <TableCell>{monitor.usdValue}</TableCell>
+                                                <TableCell>{monitor.status}</TableCell>
+                                                <TableCell>{monitor.kyc_status}</TableCell>
+                                                <TableCell>{getFormattedMethod(monitor.sourceMethod)}</TableCell>
+                                                <TableCell>{getFormattedMethod(monitor.destinationMethod)}</TableCell>
+                                            </TableRow>
+                                        )
+                                    }
+                                )
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer> </TabPanel>
+            <TabPanel value={tab} index={2}>
+                <TableContainer component={Paper}>
+                    <Table size="small">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>TransactionType</TableCell>
+                                <TableCell>Date</TableCell>
+                                <TableCell>Amount</TableCell>
+                                <TableCell>UsdValue</TableCell>
+                                <TableCell>Status</TableCell>
+                                <TableCell>Kyc Status</TableCell>
+                                <TableCell>SourceMethod</TableCell>
+                                <TableCell>DestinationMethod</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                monitorsFunding.map(monitor => {
+                                        const getFormattedMethod = (method) => {
+                                            let result = '-';
+                                            if (method != null) {
+                                                if (method.creditCard != null) {
+                                                    result = method.creditCard.first_6 + '...' + method.creditCard.last_4;
+                                                } else if (method.token != null) {
+                                                    result = method.token.address + ' (' + method.token.type + ')';
+                                                }
+                                            }
+                                            const shorten = result.substring(0, 20);
+                                            return shorten !== result ? shorten + "..." : shorten;
+                                        }
+                                        return (
+                                            <TableRow key={monitor.request_id}>
+                                                <TableCell>{monitor.transactionType}</TableCell>
+                                                <TableCell>{monitor.date.substring(0, 19)}</TableCell>
+                                                <TableCell>{monitor.amount}</TableCell>
+                                                <TableCell>{monitor.usdValue}</TableCell>
+                                                <TableCell>{monitor.status}</TableCell>
+                                                <TableCell>{monitor.kyc_status}</TableCell>
+                                                <TableCell>{getFormattedMethod(monitor.sourceMethod)}</TableCell>
+                                                <TableCell>{getFormattedMethod(monitor.destinationMethod)}</TableCell>
+                                            </TableRow>
+                                        )
+                                    }
+                                )
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </TabPanel>
+        </div>
     )
 }
 
