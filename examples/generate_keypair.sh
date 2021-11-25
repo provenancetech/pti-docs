5cc1e691f573be4222d745aefde8460bcde77cf2
Jean-Michel Bourget <jbourget@provenancetech.io>
2021-11-25 14:41:44 -0500

    Fix repository urls after repository migration (doc & urls)

--- examples/generate_keypair.sh
+++ examples/generate_keypair.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 # script to generate key material for pti api in current directory. requires access to the PTI docker registry on github
-docker run -i --rm docker.pkg.github.com/starcard-org/pti-tools/jwk-generator:latest genkey > "${1}_private.jwk"
-cat "${1}_private.jwk" | docker run -i --rm docker.pkg.github.com/starcard-org/pti-tools/jwk-generator:latest pubkey > "${1}_public.jwk"
-cat "${1}_private.jwk" | docker run -i --rm docker.pkg.github.com/starcard-org/pti-tools/jwk-generator:latest pubkey --thumbprint > "${1}_thumb.jwk"
+docker run -i --rm docker.pkg.github.com/provenancetech/pti-tools/jwk-generator:latest genkey > "${1}_private.jwk"
+cat "${1}_private.jwk" | docker run -i --rm docker.pkg.github.com/provenancetech/pti-tools/jwk-generator:latest pubkey > "${1}_public.jwk"
+cat "${1}_private.jwk" | docker run -i --rm docker.pkg.github.com/provenancetech/pti-tools/jwk-generator:latest pubkey --thumbprint > "${1}_thumb.jwk"
