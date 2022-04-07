14af0d97c192c7787d0284e1190a02681db26534
Pierre-Luc Paquin <ppaquin@provenancetech.io>
2022-04-07 17:11:28 -0400

    Adds lang to demo app.

---
+++ examples/ReactJsSdkApp/src/app/callCreateUser.js
@@ -0,0 +1,39 @@
+import { REACT_APP_BASE_URL } from "../env";
+
+const callCreateUser = async (accessToken) => {
+  const baseUrl =
+    "https://" +
+    (ptiConfig.apiDomain || "pti") +
+    (ptiConfig.ptiPrefix || "") +
+    "." +
+    REACT_APP_BASE_URL +
+    "/v0";
+
+  const url = baseUrl + "/users";
+
+  const date = new Date().toISOString();
+
+  const headers = {
+    "Content-type": "application/json",
+    Date: date,
+    "x-pti-client-id": ptiConfig.clientId,
+    "x-pti-request-id": requestId,
+    "x-pti-scenario-id": scenarioId,
+    "x-pti-session-id": ptiConfig.sessionId, // this is set via the init of the sdk
+    "x-pti-token": accessToken,
+  };
+
+  const body = { id: uuidv4(), type: "PERSON" };
+
+  const options = { method: "POST", body: JSON.stringify(body) };
+
+  const config = { headers, ...options };
+
+  return fetch(url, config).then((response) => {
+    if (response.ok) return response.json();
+
+    throw new Error(response.statusText);
+  });
+};
+
+export { callCreateUser };
