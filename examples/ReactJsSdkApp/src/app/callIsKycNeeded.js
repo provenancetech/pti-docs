14c8fbc32aa1c3591ca98cac452cca494fdd9313
Pierre-Luc Paquin <84328776+ppaquin@users.noreply.github.com>
2022-04-08 11:59:32 -0400

    Adds getBaseUrl function. (#58)

--- examples/ReactJsSdkApp/src/app/callIsKycNeeded.js
+++ examples/ReactJsSdkApp/src/app/callIsKycNeeded.js
@@ -1,13 +1,7 @@
-import { REACT_APP_BASE_URL } from "../env";
+import { getBaseUrl } from "./getBaseUrl";
 
 const callIsKycNeeded = async (accessToken) => {
-  const baseUrl =
-    "https://" +
-    (ptiConfig.apiDomain || "pti") +
-    (ptiConfig.ptiPrefix || "") +
-    "." +
-    REACT_APP_BASE_URL +
-    "/v0";
+  const baseUrl = getBaseUrl();
 
   const url = baseUrl + "/users/" + userId + "/kyc-needed?amount=" + amount;
 
