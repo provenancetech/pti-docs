b704d4fcb6fea027d64c9b355697e58a94809ad9
Jean-Michel Bourget <jbourget@provenancetech.io>
2022-09-15 10:23:05 -0400

    Added return to generateToken function.

--- examples/ReactJsSdkApp/src/repository/generateToken.js
+++ examples/ReactJsSdkApp/src/repository/generateToken.js
@@ -3,7 +3,7 @@ import { outputIfExists } from "../components/Utils";
 import { showErrorSnackAlert } from "../components/snackAlert/SnackAlert";
 
 const generateToken = async (method = "GET", url) => {
-  await callGenerateToken({ method, url })
+  return await callGenerateToken({ method, url })
     .then((token) => token?.accessToken || "")
     .catch((e) => {
       showErrorSnackAlert(`Error ${outputIfExists(e.status)} while generating token: ${outputIfExists(e.error)}`);
