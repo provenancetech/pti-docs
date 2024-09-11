d22b387dff38e19057bb42e0f37bab376648eb2c
remicartierPTI <80277900+remicartierPTI@users.noreply.github.com>
2024-09-11 10:27:34 -0400

    Keep only functioning forms (#205)

--- examples/ReactJsSdkApp/src/app/App.js
+++ examples/ReactJsSdkApp/src/app/App.js
@@ -1,42 +1,32 @@
 import React, { useEffect, useState } from "react";
 import {
   Button,
-  FormControl,
   InputAdornment,
-  InputLabel,
-  MenuItem,
-  Select,
   Stack,
   TextField,
-  Typography,
 } from "@mui/material";
 import { v4 as uuidv4 } from "uuid";
 import { LoadingButton } from "@mui/lab";
-import ReactJson from "react-json-view";
 
 import { REACT_APP_USER_ID } from "../env";
 import { actionType, paymentInfo, transactionTypes } from "../components/Consts";
-import { convertCamelCaseToText, getRandomInt } from "../components/Utils";
+import { getRandomInt } from "../components/Utils";
 import { ContainerGrid, Header, Section, Title } from "./Styles";
 import { createUser } from "../repository/createUser";
-import { callTransactionFeedback, sendTransactionFeedback } from "../repository/sendTransactionFeedback";
-import { generateTransactionLogPayload, sendTransactionLog } from "../repository/sendTransactionLog";
+import { generateTransactionLogPayload } from "../repository/sendTransactionLog";
 import { openSimpleDialog, SimpleDialog } from "../components/simpleDialog/SimpleDialog";
 import SnackAlert from "../components/snackAlert/SnackAlert";
 
 import AddCardIcon from "@mui/icons-material/AddCardOutlined";
 import TuneOutlinedIcon from "@mui/icons-material/TuneOutlined";
 import ContactPageOutlinedIcon from "@mui/icons-material/ContactPageOutlined";
-import CreditCardOutlinedIcon from "@mui/icons-material/CreditCardOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import SailingOutlinedIcon from '@mui/icons-material/SailingOutlined';
-import PaidOutlinedIcon from "@mui/icons-material/PaidOutlined";
 import OpenInNewOutlinedIcon from "@mui/icons-material/OpenInNewOutlined";
-import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 
 const App = () => {
-  const [paymentInformation, setPaymentInformation] = useState(paymentInfo.creditCard);
-  const [transactionType, setTransactionType] = useState(transactionTypes.deposit);
+  const [paymentInformation] = useState(paymentInfo.creditCard);
+  const [transactionType] = useState(transactionTypes.deposit);
   const [transactionLogPayload, setTransactionLogPayload] = useState({});
 
   const [userId, setUserId] = useState(REACT_APP_USER_ID);
@@ -46,9 +36,7 @@ const App = () => {
   const [lang, setLang] = useState("en");
 
   const [createUserLoading, setCreateUserLoading] = useState(false);
-  const [transactionLogLoading, setTransactionLogLoading] = useState(false);
-  const [transactionFeedbackLoading, setTransactionFeedbackLoading] = useState(false);
-  const [transactionFeedbackPayload, setTransactionFeedbackPayload] = useState({
+    const [transactionFeedbackPayload] = useState({
     feedback: "SETTLED",
     transactionId: "0xbb0ec8b4ab6679a0e0486f44a867dcd913cd5acee368180ac72432784eba48e4",
     date: new Date().toISOString(),
@@ -132,22 +120,7 @@ const App = () => {
           value={scenarioId}
         />
       </Section>
-
-      <Section style={{ gridArea: "payment" }}>
-        <Header>
-          <CreditCardOutlinedIcon />
-          Payment
-        </Header>
-        <Button
-          endIcon={<OpenInNewOutlinedIcon />}
-          fullWidth={true}
-          onClick={() => openSimpleDialog(actionType.funding, userId, requestId, amount, scenarioId, lang)}
-          variant="contained"
-        >
-          Open Payment Form
-        </Button>
-      </Section>
-
+        
       <Section style={{ gridArea: "kyc" }}>
         <Header>
           <ContactPageOutlinedIcon />
@@ -207,134 +180,6 @@ const App = () => {
       >
           Open Add Crypto Wallet Form
       </Button>
-      </Section>
-
-      <Section style={{ gridArea: "transaction" }}>
-        <Header>
-          <PaidOutlinedIcon />
-          Transaction
-        </Header>
-        <Stack direction="row" spacing={2}>
-          <Stack spacing={3} style={{ flex: 1 }}>
-            <FormControl>
-              <InputLabel id="transactiontype-select-label">Transaction Type</InputLabel>
-              <Select
-                id="transactiontype"
-                labelId="transactiontype-select-label"
-                fullWidth={true}
-                value={transactionType}
-                onChange={(e) => {
-                  setTransactionType(e.target.value);
-                }}
-                label="Transaction Type"
-              >
-                {Object.entries(transactionTypes).map((entry, index) => (
-                  <MenuItem key={"transactiontype-" + index} value={entry[1]}>
-                    {convertCamelCaseToText(entry[0])}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <FormControl>
-              <InputLabel id="paymentinfo-select-label">Payment Information</InputLabel>
-              <Select
-                id="paymentInformation"
-                labelId="paymentinfo-select-label"
-                fullWidth={true}
-                value={paymentInformation}
-                onChange={(e) => {
-                  setPaymentInformation(e.target.value);
-                }}
-                label="Payment Information"
-              >
-                {Object.entries(paymentInfo).map((entry, index) => (
-                  <MenuItem key={"paymentinfo-" + index} value={entry[1]}>
-                    {convertCamelCaseToText(entry[0])}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <LoadingButton
-              endIcon={<SendOutlinedIcon />}
-              fullWidth={true}
-              loading={transactionLogLoading}
-              loadingPosition="end"
-              onClick={() => {
-                setTransactionLogLoading(true);
-                sendTransactionLog(props).finally(() => {
-                  setTransactionLogLoading(false);
-                });
-              }}
-              variant="contained"
-            >
-              Send Transaction Log
-            </LoadingButton>
-          </Stack>
-
-          <FormControl style={{ minWidth: "600px" }}>
-            <Typography component="p">Transaction Log Payload</Typography>
-            <ReactJson
-              collapsed={2}
-              displayDataTypes={false}
-              displayObjectSize={false}
-              enableClipboard={true}
-              iconStyle={"triangle"}
-              id="transactionLogPayload"
-              onEdit={(value) => {
-                if (value?.updated_src) setTransactionLogPayload(value.updated_src);
-              }}
-              src={transactionLogPayload}
-              theme={"rjv-default"}
-              style={{ fontSize: "14px", padding: "20px" }}
-            />
-          </FormControl>
-        </Stack>
-      </Section>
-
-      <Section style={{ gridArea: "feedback" }}>
-        <Header>
-          <PaidOutlinedIcon />
-          Transaction Feedback
-        </Header>
-        <Stack direction="row" spacing={2}>
-          <Stack spacing={3} style={{ flex: 1 }}>
-            <LoadingButton
-              endIcon={<SendOutlinedIcon />}
-              fullWidth={true}
-              loading={transactionFeedbackLoading}
-              loadingPosition="end"
-              onClick={() => {
-                setTransactionFeedbackLoading(true);
-                sendTransactionFeedback(props)
-                  .then(() => {})
-                  .finally(() => {
-                    setTransactionFeedbackLoading(false);
-                  });
-              }}
-              variant="contained"
-            >
-              Send Transaction Feedback
-            </LoadingButton>
-          </Stack>
-
-          <FormControl style={{ minWidth: "600px" }}>
-            <Typography component="p">Transaction Feedback Payload</Typography>
-            <ReactJson
-              collapsed={2}
-              displayDataTypes={false}
-              displayObjectSize={false}
-              enableClipboard={true}
-              iconStyle={"triangle"}
-              id="transactionFeedbackPayload"
-              onEdit={(value) => {
-                if (value?.updated_src) setTransactionFeedbackPayload(value.updated_src);
-              }}
-              src={transactionFeedbackPayload}
-              theme={"rjv-default"}
-              style={{ fontSize: "14px", padding: "20px" }}
-            />
-          </FormControl>
-        </Stack>
       </Section>
 
       <SimpleDialog />
