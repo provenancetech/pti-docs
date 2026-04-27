e5cec861e83c0891f2e672ad90333090a0782767
Rémi Cartier <rcartier@provenancetech.io>
2026-04-27 14:47:37 -0400

    Fix sample app not starting

--- examples/ReactJsSdkApp/craco.config.js
+++ examples/ReactJsSdkApp/craco.config.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+    eslint: {
+        configure: (eslintConfig) => {
+            // Remove the options that cause the crash
+            delete eslintConfig.extensions;
+            delete eslintConfig.resolvePluginsRelativeTo;
+
+            return eslintConfig;
+        },
+    },
+};
